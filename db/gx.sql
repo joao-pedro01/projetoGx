dc300a7e74def61da388eb445afecb38adf7a185
João Pedro <t_jopedro@prefeitura.local>
2022-09-16 17:02:10 -0300

    Atualizado tabelas

--- db/gx.sql
+++ db/gx.sql
@@ -1,33 +1,15 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           10.9.2-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              11.3.0.6295
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Copiando estrutura do banco de dados para gx
-CREATE DATABASE IF NOT EXISTS `gx` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE IF NOT EXISTS `gx`;
 USE `gx`;
 
--- Copiando estrutura para tabela gx.atributos
+-- tabela atributos
 CREATE TABLE IF NOT EXISTS `atributos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(150) DEFAULT NULL,
-  `is_active` tinyint(1) DEFAULT NULL,
+  `nome` varchar(150) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Exportação de dados foi desmarcado.
-
--- Copiando estrutura para tabela gx.equipamentos
+-- tabela equipamentos
 CREATE TABLE IF NOT EXISTS `equipamentos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(150) NOT NULL,
@@ -35,10 +17,25 @@ CREATE TABLE IF NOT EXISTS `equipamentos` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Exportação de dados foi desmarcado.
+-- tabela pecas
+CREATE TABLE IF NOT EXISTS `pecas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela gx.equipamentos_atributos
+-- tabela usuarios
+CREATE TABLE IF NOT EXISTS `usuarios`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`nome` VARCHAR (150) NOT NULL,
+`senha` CHAR (150) NOT NULL,
+PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- tabela fk equipamentos_atributos
 CREATE TABLE IF NOT EXISTS `equipamentos_atributos` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `id_equipamentos` int(11) NOT NULL,
   `id_atributos` int(11) NOT NULL,
   KEY `FK__equipamentos` (`id_equipamentos`),
@@ -47,31 +44,13 @@ CREATE TABLE IF NOT EXISTS `equipamentos_atributos` (
   CONSTRAINT `FK__equipamentos` FOREIGN KEY (`id_equipamentos`) REFERENCES `equipamentos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Exportação de dados foi desmarcado.
-
--- Copiando estrutura para tabela gx.pecas
-CREATE TABLE IF NOT EXISTS `pecas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(150) NOT NULL,
-  `is_active` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-
--- tabela usuarios
-
-CREATE TABLE IF NOT EXISTS 'usuarios'(
-
-'nome' VARCHAR (150) NOT NULL AUTO_INCREMENT,
-'senha' CHAR (150) NOT NULL, 
-'id' int(11) NOT NULL,
-
-PRIMARY KEY ('id_usuario')
-
-)ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-
--- Exportação de dados foi desmarcado.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+-- tabela fk pecas_atributos
+CREATE TABLE IF NOT EXISTS `pecas_atributos` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `id_pecas` int(11) NOT NULL,
+  `id_atributos` int(11) NOT NULL,
+  KEY `FK__pecas` (`id_pecas`),
+  KEY `FK__atributos` (`id_atributos`),
+  FOREIGN KEY (`id_atributos`) REFERENCES `atributos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (`id_pecas`) REFERENCES `pecas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
