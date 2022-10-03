9383ca04592fe6238431026a7eec721f42ce4722
jabrantes <joaotrabachini@gmail.com>
2022-10-03 15:08:04 -0300

    Foi organizado em pastas o código, criação da lógica de login, criação da main page, App atualizado com as rotas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,49 +1,24 @@
-<<<<<<< HEAD
-import pms from './assets/logo_pms.png' // import logo prefeitura
-import './stylesLogin.css'  //import estilizacao css
-import bootstrap from 'bootstrap' //import estilizacao bootstrap
-=======
-import pms from './assets/logo_pms.png'
-import './styles.css'
->>>>>>> 205708b06c21e674553debeb58cc9770fff5cbcf
+import React from 'react';
+import Login from './pages/login/indexLogin'
+import MainPage from './pages/mainpage/indexMP'
 
-function App() {
+import { 
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+const App = () => {
   return ( 
-    <div className="container">
-      <div className="boxLogin">
-<<<<<<< HEAD
-        <div className="login"> 
-          <form className="formLogin" action="#" method="post"> {/* capturar dados do form */}
-          <span className="loginTitulo"><img src= {pms} className='img-fluid'></img></span> {/* imagem pms */} 
-            <h1 className="loginTitulo">ESTOQUE</h1> {/* titulo da pagina */}
-            <div className='inputsL'> {/* div lista de inputs */}
-              <input className="input" type="text" placeholder="Nome de usuário" />
-            </div>
-            <div className='inputsL'>
-              <input className="input" type="password" placeholder="Senha" />
-            </div>
-            <div className='containerLoginBotao'>
-              <button type="button" class="btn btn-primary btn-block">Entrar</button>
-=======
-        <div className="login">
-          <form className="formLogin">
-          <span className="loginTitulo"><img src= {pms} alt="Prefeitura Municipal de Sorocaba"></img></span>
-            <h1 className="loginTitulo">Bem vindo</h1>
-            <div className='inputsL'>
-              <input className="input" type="email" />
-            </div>
-            <div className='inputsL'>
-              <input className="input" type="password" />
-            </div>
-            <div className='containerLoginBotao'>
-            <button className="loginBotao"> Login </button>
->>>>>>> 205708b06c21e674553debeb58cc9770fff5cbcf
-            </div>
-          </form>
-        </div>
-      </div>
+    <div className="app">
+      <Router> {/* Sistema de rotas da pagina */}
+        <Routes>
+          <Route exact path="/" element={<Login />} />   
+          <Route exact path="/pecas" element={<MainPage />} />
+        </Routes>
+      </Router>
     </div>
-  
   );
   }
   
---
+++ front-end/src/pages/login/indexLogin.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+import pms from './assets/logo_pms.png' // import logo prefeitura
+import './stylesLogin.css'  //import estilizacao css
+import bootstrap from 'bootstrap' //import estilizacao bootstrap
+
+const Login = () => { 
+    const[user, setUser] = useState(""); {/* definicao da variavel user e password (como npadrao vazio) para autenticacao */}
+    const[password, setPassword] = useState("");
+
+    const handleSubmit = (event) => { {/* cria o evento para fazer o submit dos dados user e password*/}
+         event.preventDefault();
+        console.log("submit", {user, password});
+    }
+    return (
+        <div className="container">
+            <div className="boxLogin">
+            <div className="login"> 
+                <form className="formLogin" action="#" method="post" onSubmit={handleSubmit}> {/* capturar dados do form */}
+                <span className="loginTitulo"><img src= {pms} className='img-fluid'></img></span> {/* imagem pms */} 
+                <h1 className="loginTitulo">ESTOQUE</h1> {/* titulo da pagina */}
+                <div className='inputsL'> {/* div lista de inputs */}
+                    <input className="input" name="nome" type="text" placeholder="Nome de usuário" required
+                    value = {user} onChange = {(event) => setUser(event.target.value)} />  {/* define o input digitado para a const user*/}
+                </div>
+                <div className='inputsL'>
+                    <input className="input" type="password" placeholder="Senha"
+                    value = {password} onChange = {(event) => setPassword(event.target.value)}/> {/* define o input digitado para a const password*/}
+                </div>
+                <div className='containerLoginBotao'>
+                    <button type="submit" class="btn btn-primary btn-block" >Entrar</button>
+                </div>
+                </form>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Login;
