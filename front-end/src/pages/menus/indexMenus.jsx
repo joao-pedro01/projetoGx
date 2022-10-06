14c3c2e9a644ad78e3c0df3238c2968885e428b1
Felipe Pinheiro Lopes <t_fplopes@prefeitura.local>
2022-10-06 16:11:34 -0300

    Botão menu

--- front-end/src/pages/menus/indexMenus.jsx
+++ front-end/src/pages/menus/indexMenus.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button'
 import HeaderPMS from '../header/indexHeader'
+import './stylesMenus.css'  //import estilizacao css
 
 const Menus = () => { 
     return (
         <div className='MenuContainer'> 
-            <HeaderPMS/>
-            <Button variant="primary">Adicionar</Button>{' '}
-            <Button variant="success">Remover</Button>{' '}
-            <Button variant="warning">Listagem</Button>{' '}
-            <Button variant="danger">Relatórios</Button>{' '}
-            <div className='buttonOne'>
-                <a href="/pecas"><h1> Adicionar </h1></a>
+            <div>
+                <HeaderPMS/>
             </div>
+
+            <div className='top'>
+                <a className='buttonOne' href='/menu'><div className='teste'>__Peças__</div></a>
+                <a className='buttonTwo' href='/menu'><div className='teste'>__Peças__</div></a>
+            </div>
+            <div className='down'>
+                <a className='buttonThree' href='/menu'><div className='teste'>__Peças__</div></a>
+                <a className='buttonFour' href='/menu'><div className='teste'>__Peças__</div></a>
+            </div>
+
         </div>
     )
 }
