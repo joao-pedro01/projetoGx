ee506a8e2d9bfaa5bb4b9b2a6b3c89f22d7bd045
Felipe Pinheiro Lopes <t_fplopes@prefeitura.local>
2022-10-07 13:39:25 -0300

    Botões do menu

--- front-end/src/pages/menus/indexMenus.jsx
+++ front-end/src/pages/menus/indexMenus.jsx
@@ -5,18 +5,18 @@ import './stylesMenus.css'  //import estilizacao css
 
 const Menus = () => { 
     return (
-        <div className='MenuContainer'> 
+        <div className='MenuContainer'>
             <div>
                 <HeaderPMS/>
             </div>
 
             <div className='top'>
-                <a className='buttonOne' href='/menu'><div className='teste'>__Peças__</div></a>
-                <a className='buttonTwo' href='/menu'><div className='teste'>__Peças__</div></a>
+                <a className='buttonOne' href='/menu'><div className='text-button'>Cadastrar Peças</div></a>
+                <a className='buttonTwo' href='/menu'><div className='text-button'>Retirar Peças</div></a>
             </div>
             <div className='down'>
-                <a className='buttonThree' href='/menu'><div className='teste'>__Peças__</div></a>
-                <a className='buttonFour' href='/menu'><div className='teste'>__Peças__</div></a>
+                <a className='buttonThree' href='/peca'><div className='text-button'>Listar Peças</div></a>
+                <a className='buttonFour' href='/menu'><div className='text-button'>Relatório</div></a>
             </div>
 
         </div>
