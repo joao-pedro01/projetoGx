98498a1e7803390b407db2aef0530fd920703673
jabrantes <joaotrabachini@gmail.com>
2022-10-07 13:51:33 -0300

    axios get funcionando

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import Login from './pages/login/indexLogin'
 import MainPage from './pages/listaPecas/indexMP'
 import Header from './pages/header/indexHeader'
 import Menus from './pages/menus/indexMenus'
+import Test from './test/test'
 //     <Router> {/* Sistema de rotas da pagina */}
 // <Routes>
 // <Route exact path="/" element={<Login />} />   
@@ -17,6 +18,7 @@ import {
   Link,
 } from "react-router-dom";
 
+
 const App = () => {
   return ( 
     <div className="app">
@@ -24,8 +26,9 @@ const App = () => {
           <Routes>
             <Route exact path="/" element={<Login />} />   
             <Route exact path="/pecas" element={<MainPage />} />
-            <Route exact path="/teste" element={<Header/>}/>
+            <Route exact path="/testeHeader" element={<Header/>}/>
             <Route exact path="/menu" element={<Menus/>}/>
+            <Route exact path="/teste" element={<Test/>}/>
           </Routes>
         </Router>
     </div>
