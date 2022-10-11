60484abfa1f18303e2e506ad738272cab3ceff87
joao-pedro01 <joao_pedro01@terra.com.br>
2022-10-11 11:17:02 -0300

    Add swagger (documentação)

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "../../helpers/swagger.json" assert { type: "json" };
 import usuarios from "./UsuariosRoutes.js";
 import pecas from "./PecasRoutes.js";
 
@@ -10,6 +12,7 @@ const routes = (app) => {
     app.route('/api/test').get((req,res) => {
         res.status(200).json({test: 1});
     });
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
     // rotas de arquivos externos
     app.use((req, res, next) => {
         res.append('Access-Control-Allow-Origin', '*');
