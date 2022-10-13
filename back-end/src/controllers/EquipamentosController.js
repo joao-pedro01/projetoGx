0a4d77a049ae84c45b1e0c9ffa5bcfd59f05b3ea
joao-pedro01 <joao_pedro01@terra.com.br>
2022-10-13 11:38:04 -0300

    Removido o que não estava sendo utilizado ainda

--- back-end/src/controllers/EquipamentosController.js
+++ back-end/src/controllers/EquipamentosController.js
@@ -3,33 +3,6 @@ import { dd } from './functions.js';
 
 // class responsavel por todas acoes das pecas
 class EquipamentosController {
-    // function que retorna todas as pecas
-    static equipamento = (req, res) => {
-        var id = req.params.id;
-        var select = equipamento(id);
-
-        select.then((peca) => {
-            if(peca.length === 0) {
-                res.status(404).json("Peça não encontrada!!!");
-            }else {
-                var innerJoin = peca_atributos(id);
-                
-                innerJoin.then((atributos) => {
-                    if(atributos.length === 0) {
-                        res.status(200).json(peca);
-                        console.log("Não tem atributos");
-                    }else {
-                        var result = { peca, atributos }
-                        res.status(200).json(result);
-                    }
-                }).catch(err => {
-                    console.log(err);
-                    res.status(500).send({message: `falha ao exibir peça`});
-                })
-            };
-        });
-    };
-
     static listarEquipamentos = (req, res) => {
         var status = req.query.status, query;
 
@@ -60,73 +33,6 @@ class EquipamentosController {
                 res.status(500).send({message: `falha ao listar peças com query`});
             });
         };
-    };
-
-    static cadastrarPeca = (req, res) => {
-        let peca = req.body;
-
-        if(peca.nome == undefined || peca.sku == undefined){
-            res.status(422).send({message: 'input indefinido'});
-        }else {
-            var insert = cadastrarPeca(peca);
-
-            insert.then(() => {
-                res.status(200).send({message:  `Peça foi cadastrada com sucesso`});
-            }).catch(err => {
-                console.log(err);
-                res.status(500).send({message: `falha ao cadastrar peça`});
-            });
-        };
-    };
-
-
-    static alterarQuantidade = (req, res) => {        
-        var id = req.params.id;
-        var select = peca(id);
-        
-        select.then((peca) => {
-            var qnt = req.body.qnt;
-            // se a peca nao existir vai entrar no if
-            if(peca.length == 0) {
-                res.status(404).send({message: "peça não encontrada"});
-            }else if(peca[0].is_active == false) {
-                res.status(405).json("Peça esta desativada não pode alterar");
-            }else if(peca[0].qnt + qnt < 0) {
-                res.status(400).send({message: 'Não é possivel pois não tem a quantidade em estoque'});
-            }else {
-                var qnt = peca[0].qnt + qnt;
-                var update = alterarQuantidade(id, qnt);
-                update.then(() => {
-                    res.status(200).json(`${peca[0].nome} foi alterado no estoque para: ${qnt}`);
-                }).catch(err => {
-                    console.log(err);
-                    res.status(500).send({message: `falha ao atualizar a quantidade da peça`});
-                });
-            };
-        });
-    };
-
-    static desativarPeca = (req, res) => {
-        var id = req.params.id;
-        var select = peca(id);
-
-        select.then((peca) => {
-            // se a peca nao existir vai entrar no if
-            if(peca.length == 0) {
-                res.status(404).json("Peça não existe!!!");
-            }else if(peca[0].is_active == false) {
-                res.status(405).json("Peça já esta desativada");
-            }else {
-                var update = desativarPeca(id);
-                // caso passar por todos os if ira desativar a peca
-                update.then(() => {
-                    res.status(200).json(`${peca[0].nome} desativada com sucesso`);
-                }).catch(err => {
-                    console.log(err);
-                    res.status(500).send({message: `falha ao desativar peça`});
-                });
-            };
-        });
     };
 };
 
--- back-end/src/models/Equipamentos.js
+++ back-end/src/models/Equipamentos.js
@@ -9,25 +9,3 @@ export const listarEquipamentos = async(params) => {
     return await conn.select('*').table('equipamentos').where(params);
   }
 }
-
-export const equipamento = async(id) => {
-  return await conn.select('nome','patrimonio', 'is_active').table('equipamentos').where('id', id);
-}
-
-export const peca_atributos = async(id) => {
-  return await conn.select().table('equipamentos_atributos')
-    .innerJoin( 'atributos', 'atributos.id', 'equipamentos_atributos.id_atributo')
-    .where('equipamentos_atributos.id_peca', id);
-}
-
-export const cadastrarPeca = async(dados) => {
-  return await conn.insert(dados).into("equipamentos");
-}
-
-export const desativarPeca = async(id) => {
-  return await conn.where({id: id}).update({is_active: false}).table('equipamentos');
-}
-
-export const alterarQuantidade = async(id, value) => {
-  return await conn.update({ qnt: value }).where({id: id}).table('equipamentos');
-}
