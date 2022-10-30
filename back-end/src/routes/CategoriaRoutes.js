66e8f70805f05d106f38d74d172eb112f6e8e565
joao-pedro01 <joao_pedro01@terra.com.br>
2022-10-30 10:34:46 -0300

    Add post e delete

--- back-end/src/routes/CategoriaRoutes.js
+++ back-end/src/routes/CategoriaRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 // rotas para acoes usuarios
 router
     .get("/api/categorias/", CategoriaController.listarCategorias)
+    .post("/api/categorias", CategoriaController.cadastrarCategoria)
+    .delete("/api/categorias/:id", CategoriaController.desativarCategoria)
 
 export default router;
