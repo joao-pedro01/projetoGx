f191c9ff19968d923aeb98cf19f52f2ecc3a95c4
joao-pedro01 <joao_pedro01@terra.com.br>
2022-12-08 14:43:02 -0300

    Add function removeNull

--- back-end/src/controllers/functions.js
+++ back-end/src/controllers/functions.js
@@ -3,7 +3,18 @@ import var_dump from "var_dump";
 export function dd(params) {
     var_dump(params);
     process.exit();
-};
+}
+
+export function removeNull(Object) {    
+    Object.map(obj => {
+        for(const column in obj) {
+            if(obj[column] == null) {
+                console.log(column, '=', obj[column]);
+                delete obj[column];
+            }
+        }
+    });
+}
 
 export function removeUndefined(obj) {
     Object.keys(obj).forEach(key => {
@@ -11,4 +22,4 @@ export function removeUndefined(obj) {
             delete obj[key];
         };
     });
-};
+}
